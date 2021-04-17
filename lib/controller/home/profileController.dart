2c01ad399f77ec9d93eb04ea7385d190d4fb72eb
Praveen Geevinda <praveen.2018836@iit.ac.lk>
2021-04-17 10:22:55 +0530

    Modified profile page to fetch data from aws

--- lib/controller/home/profileController.dart
+++ lib/controller/home/profileController.dart
@@ -1,5 +1,7 @@
+import 'package:amr_mobile/domain/ElectricityAccount.dart';
 import 'package:amr_mobile/routes/pages.dart';
 import 'package:amr_mobile/service/authService.dart';
+import 'package:amr_mobile/service/httpService.dart';
 import 'package:amr_mobile/utils/constants.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -8,6 +10,12 @@ class ProfileController extends GetxController {
   GetStorage storage = Get.find();
   var loading = false.obs;
   var name = ''.obs;
+  var email = ''.obs;
+  var mobile = ''.obs;
+  var nic = ''.obs;
+  Rx<ElectricityAccount> account;
+
+  final HttpService _httpProvider = Get.find();
   final AuthService _authService = Get.find();
 
   @override
@@ -19,9 +27,35 @@ class ProfileController extends GetxController {
   void getUserDetails() async {
     loading.value = true;
     var userattributes = await _authService.getUserAttributes();
+
+    // fetch name
     var nameAttribute = userattributes
         .firstWhere((element) => element.userAttributeKey == 'name');
     name.value = nameAttribute.value;
+
+    // fetch email
+    var emailAttribute = userattributes
+        .firstWhere((element) => element.userAttributeKey == 'email');
+    email.value = emailAttribute.value;
+
+    // fetch mobile
+    var mobileAttribute = userattributes
+        .firstWhere((element) => element.userAttributeKey == 'phone_number');
+    mobile.value = mobileAttribute.value;
+
+    // fetch nic
+    var nicAttribute = userattributes
+        .firstWhere((element) => element.userAttributeKey == 'custom:nic');
+    nic.value = nicAttribute.value;
+
+    // fetch electricty account no
+    var user = await _authService.getUserDetails();
+    var res = await _httpProvider.getRequest('/account/getAccountDetails',
+        params: {'userId': user.userId});
+    if (res != null) {
+      account = ElectricityAccount.fromJson(res.body[0]).obs;
+    }
+
     loading.value = false;
   }
 
