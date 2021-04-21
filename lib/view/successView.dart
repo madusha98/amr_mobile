b66159de7367e160558f026f7bfc5f75fb2a480d
Ruvindra Senevirathne <ruvindra.2018848@iit.ac.lk>
2021-04-21 17:23:58 +0530

    Reduce bill info banners added

--- lib/view/successView.dart
+++ lib/view/successView.dart
@@ -3,8 +3,24 @@ import 'package:amr_mobile/controller/home/bottomNavController.dart';
 import 'package:amr_mobile/controller/successController.dart';
 import 'package:amr_mobile/routes/pages.dart';
 import 'package:amr_mobile/utils/constants.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+final List<String> imagesList = [
+  'https://cdn.pixabay.com/photo/2020/11/01/23/22/breakfast-5705180_1280.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/18/19/00/breads-1836411_1280.jpg',
+  'https://cdn.pixabay.com/photo/2019/01/14/17/25/gelato-3932596_1280.jpg',
+  'https://cdn.pixabay.com/photo/2017/04/04/18/07/ice-cream-2202561_1280.jpg',
+];
+
+final List<String> imgList = [
+  'assets/tip1.png',
+  'assets/tip2.png',
+  'assets/tip3.png',
+  'assets/tip4.png',
+  'assets/tip5.png'
+];
 
 class Success extends GetView<SuccessController> {
   @override
@@ -19,7 +35,7 @@ class Success extends GetView<SuccessController> {
         backgroundColor: BACKGROUND_COLOR,
         body: Padding(
           padding: const EdgeInsets.only(
-              left: PADDING, right: PADDING, bottom: 40.0),
+              left: PADDING, right: PADDING, bottom: PADDING),
           child: Obx(
             () => controller.loading.value
                 ? Center(
@@ -29,8 +45,8 @@ class Success extends GetView<SuccessController> {
                     children: [
                       Expanded(
                         child: Padding(
-                            padding:
-                                const EdgeInsets.only(top: 60.0, bottom: 30.0),
+                            padding: const EdgeInsets.only(
+                                top: PADDING * 2, bottom: PADDING),
                             child: Container(
                                 height: Get.height -
                                     135 -
@@ -41,12 +57,12 @@ class Success extends GetView<SuccessController> {
                                   children: [
                                     Image.asset(
                                       'assets/success.png',
-                                      height: 100.0,
+                                      height: 60.0,
                                       fit: BoxFit.cover,
                                     ),
                                     Padding(
                                       padding: const EdgeInsets.only(
-                                          top: PADDING * 2, bottom: PADDING),
+                                          top: PADDING, bottom: PADDING),
                                       child: Text(
                                         'Success!',
                                         style: Get.textTheme.headline6.apply(
@@ -81,6 +97,82 @@ class Success extends GetView<SuccessController> {
                                       style: TextStyle(
                                           color: Colors.black.withOpacity(0.5),
                                           fontSize: 14),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: PADDING * 2),
+                                      child: Text(
+                                        'Here are some tips to reduce your next month bill :',
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(
+                                            color: Get.theme.accentColor,
+                                            fontSize: 13),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(top: PADDING),
+                                      child: Container(
+                                          height: 95.0,
+                                          width: Get.width,
+                                          color: Colors.transparent,
+                                          child: CarouselSlider(
+                                            options: CarouselOptions(
+                                              autoPlay: true,
+                                              enlargeCenterPage: true,
+                                            ),
+                                            items: imgList
+                                                .map((item) => Container(
+                                                      height: 120,
+                                                      color: Colors.transparent,
+                                                      child: Center(
+                                                        child: Padding(
+                                                          padding:
+                                                              const EdgeInsets
+                                                                  .all(5.0),
+                                                          child: Container(
+                                                            margin:
+                                                                const EdgeInsets
+                                                                        .fromLTRB(
+                                                                    3, 0, 3, 0),
+                                                            child: ClipRRect(
+                                                                borderRadius:
+                                                                    BorderRadius
+                                                                        .circular(
+                                                                            8),
+                                                                child:
+                                                                    Image.asset(
+                                                                  item,
+                                                                  fit: BoxFit
+                                                                      .fitWidth,
+                                                                  height: 120,
+                                                                  width: 240,
+                                                                )),
+                                                            decoration: BoxDecoration(
+                                                                color:
+                                                                    Colors.grey,
+                                                                shape: BoxShape
+                                                                    .rectangle,
+                                                                borderRadius: BorderRadius
+                                                                    .all(Radius
+                                                                        .circular(
+                                                                            8.0)),
+                                                                boxShadow: [
+                                                                  BoxShadow(
+                                                                    color: Colors
+                                                                        .green,
+                                                                    blurRadius:
+                                                                        3.0,
+                                                                    spreadRadius:
+                                                                        2.0,
+                                                                  ),
+                                                                ]),
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ))
+                                                .toList(),
+                                          )),
                                     ),
                                   ],
                                 ))),
